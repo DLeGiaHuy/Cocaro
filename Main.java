a5c2db7b7876c72a3154c8d90a6bf631459b0706
23130428-lab <23130428@st.hcmuaf.edu.vn>
2026-06-15 22:59:04 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+package carogame;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new CaroGUI();
+        });
+    }
+}
